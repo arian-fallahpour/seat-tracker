3416ddb250bc9a1a1e801113e500403e97b1ad0f
JinXzAF <arianf2004@gmail.com>
2025-02-07 22:02:11 -0500

    added campus enum values

--- data/enums.js
+++ data/enums.js
@@ -2,5 +2,10 @@ module.exports = {
   schools: ["uoft", "waterloo", "western"],
   section: {
     type: ["tutorial", "lab"],
+    campus: {
+      UoftSection: ["Scarborough", "Mississauga", "St. George"],
+      WesternSection: ["Main", "King's", "Huron", "Brescia"],
+      WaterlooSection: [],
+    },
   },
 };
