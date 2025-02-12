05a1ccf9635432764d920aa86e9e97927c82767c
JinXzAF <arianf2004@gmail.com>
2025-02-11 23:06:32 -0500

    simplified and optimized

--- scripts/import-uoft-courses.js
+++ scripts/import-uoft-courses.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const args = require("args-parser")(process.argv);
 
 const Section = require("../models/database/Section/Section");
 const Course = require("../models/database/Course/Course");
 const UoftAdapter = require("../models/api-adapters/UoftAdapter");
-const UoftSection = require("../models/database/Section/UoftSection");
+const courseController = require("../controllers/courseController");
 
 dotenv.config({ path: "./config.env" });
 
@@ -16,59 +17,31 @@ dotenv.config({ path: "./config.env" });
   await mongoose.connect(dbUri, { autoIndex: true });
 
   // Run script
-  if (process.argv[2] === "--import") {
-    await importData();
-  } else if (process.argv[2] === "--delete") {
+  if (args.delete === "--delete") {
     await deleteData();
+  } else {
+    await importData();
   }
   process.exit();
 })();
 
 const importData = async () => {
-  const limit = -1;
+  const updatedCourses = [];
+
   let page = 0;
-  while (page !== limit) {
-    console.log(`Requestion UofT API page ${++page}`);
+  const limit = args.limit || -1;
+  while (page != limit) {
+    console.log(`[INFO] Requesting UofT API (page: ${++page})`);
 
     // Fetch courses from school API
     const fetchedCourses = await UoftAdapter.getCourses({ page });
     if (fetchedCourses.length === 0) break;
 
-    // Separate course and section data into parallel arrays
-    const coursesData = fetchedCourses.map((course) => ({
-      ...course,
-      sections: [],
-    }));
-    const sectionsData = fetchedCourses.map((course) => course.sections);
-
-    try {
-      // Create new courses in parallel, skip import of courses that cause errors
-      const courses = await Course.insertMany(coursesData, { ordered: false });
-
-      // Add id to sections and flatten them
-      const flattenedSectionsData = sectionsData
-        .map((sections, i) =>
-          sections.map((section) => ({
-            ...section,
-            course: courses[i]._id,
-            courseIndex: i,
-          }))
-        )
-        .flat();
-
-      // Create new sections in parallel
-      const sections = await UoftSection.insertMany(flattenedSectionsData);
-
-      // Add section ids to sections of courses, and save in parallel
-      sections.forEach((section, i) => {
-        const index = flattenedSectionsData[i].courseIndex;
-        courses[index].sections.push(section._id);
-      });
-      await Course.bulkSave(courses);
-    } catch (err) {
-      console.error(`Error: ${err.message}`);
-    }
+    updatedCourses.push(...fetchedCourses);
   }
+
+  // Upsert courses and sections
+  await courseController.upsertUoftCoursesAndSections(updatedCourses);
 };
 
 const deleteData = async () => {
