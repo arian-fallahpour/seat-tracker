0f393b6f85ca7645dfbd629cc7c4f41877eff9ae
JinXzAF <arianf2004@gmail.com>
2025-02-11 23:05:14 -0500

    replaced update function with upsert, and removed unimplemented functions

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,6 +1,6 @@
 const { processUoftAlert, processWaterlooAlert } = require("./processorController");
 const Alert = require("../models/database/Alert");
-const Course = require("../models/database/Course/Course");
+const courseController = require("../controllers/courseController");
 
 exports.scheduleUoftAlerts = async () => {
   try {
@@ -15,36 +15,9 @@ exports.scheduleUoftAlerts = async () => {
     });
 
     // Update course data from new fetched course data
-    const updatedCourses = courseCache.values();
-    await Course.updateCoursesBulk(updatedCourses);
+    const updatedCourses = Array.from(courseCache.values());
+    await courseController.upsertUoftCoursesAndSections(updatedCourses);
   } catch (err) {
     console.error(`[ERROR] Uoft Schedule Error: ${err.message}`);
-  }
-};
-
-exports.scheduleWaterlooAlerts = async () => {
-  try {
-    // Find all active alerts for waterloo
-    const alerts = await Alert.findActiveAlerts("waterloo");
-    if (alerts.length === 0) return;
-
-    // Process alerts using waterloo processor
-    const courseCache = await Alert.processAlerts(alerts, {
-      processor: processWaterlooAlert,
-      throttleDelay: process.env.WATERLOO_ALERTS_THROTTLE_MS,
-    });
-
-    // Update course data from new fetched course data
-    const updatedCourses = courseCache.values();
-    await Course.updateCoursesBulk(updatedCourses);
-  } catch (err) {
-    console.error(`[ERROR] Waterloo Schedule Error: ${err.message}`);
-  }
-};
-
-exports.scheduleWesternAlerts = async () => {
-  try {
-  } catch (err) {
-    console.error(`[ERROR] Western Schedule Error: ${err.message}`);
   }
 };
