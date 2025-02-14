a05afea136a35f4c094c5a52a3babae5a727f977
JinXzAF <arianf2004@gmail.com>
2025-02-13 20:58:48 -0500

    changed enum naming

--- data/enums.js
+++ data/enums.js
@@ -1,10 +1,12 @@
 module.exports = {
+  alert: {
+    school: ["uoft", "waterloo"],
+  },
   section: {
     type: ["tutorial", "lab"],
     deliveryMode: ["in person", "hybrid", "online"],
     campus: {
       UoftSection: ["Scarborough", "Mississauga", "St. George"],
-      WesternSection: ["Main", "King's", "Huron"],
       WaterlooSection: [],
     },
   },
