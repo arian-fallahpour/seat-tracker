1fe7f700fe3ba1ec530e49068eea83f20b31a5d4
JinXzAF <arianf2004@gmail.com>
2025-02-13 20:58:33 -0500

    moved database connection and added scheduler initialization

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const scheduler = require("./controllers/scheduler");
 
 process.on("uncaughtException", (err) => {
   console.error(`[ERROR] (Uncaught Exception) ${err.stack}`);
@@ -12,18 +13,20 @@ dotenv.config({ path: "./config.env" });
 // App initialization
 const app = require("./app");
 
-// Database initialization
-let dbUri = process.env.DATABASE_CONNECTION;
-dbUri = dbUri.replace("<DATABASE_USER>", process.env.DATABASE_USER);
-dbUri = dbUri.replace("<DATABASE_PASS>", process.env.DATABASE_PASS);
-mongoose
-  .connect(dbUri, { autoIndex: true })
-  .then(() => console.log("[INFO] Database connection successful"));
-
 // Server initialization
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
+const server = app.listen(port, async () => {
   console.log(`[INFO] App running on port ${port}`);
+
+  // Database initialization
+  let dbUri = process.env.DATABASE_CONNECTION;
+  dbUri = dbUri.replace("<DATABASE_USER>", process.env.DATABASE_USER);
+  dbUri = dbUri.replace("<DATABASE_PASS>", process.env.DATABASE_PASS);
+  await mongoose.connect(dbUri, { autoIndex: true });
+  console.log("[INFO] Database connection successful");
+
+  // Scheduler initialization
+  scheduler.init();
 });
 
 process.on("unhandledRejection", (err) => {
