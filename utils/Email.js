6eefba2ac491187dd171b4dfb045d5e0a57b2185
JinXzAF <arianf2004@gmail.com>
2025-03-07 00:15:11 -0500

    created utility classes

--- utils/Email.js
+++ utils/Email.js
@@ -1,13 +1,68 @@
+const businessData = require("../data/business-data");
 const formData = require("form-data");
 const Mailgun = require("mailgun.js");
 
+// TODO: Setup for production
 class Email {
   constructor() {
+    this.from = `${businessData.name} <alerts@${process.env.MAILGUN_DOMAIN}>`;
+
     const mailgun = new Mailgun(formData);
     this.mailgun = mailgun.client({
       username: "api",
       key: process.env.MAILGUN_API_KEY,
     });
+  }
+
+  toEmail(email) {
+    this.to = [email];
+    return this;
+  }
+
+  toEmails(emails) {
+    this.to = emails;
+    return this;
+  }
+
+  withSubject(subject) {
+    this.subject = subject;
+    return this;
+  }
+
+  withTemplate(template, data) {
+    if (template === "alert-notify") {
+      this.withTemplateAlertNotify(data);
+    } else if (template === "alert-activate") {
+      this.withTemplateAlertActivate(data);
+    }
+
+    return this;
+  }
+
+  // TODO: Fix this
+  withTemplateAlertNotify(data) {
+    this.text = data.text;
+    this.html = data.html;
+    return this;
+  }
+
+  // TODO: Fix this
+  withTemplateAlertActivate(data) {
+    this.text = data.text;
+    this.html = data.html;
+    return this;
+  }
+
+  async send() {
+    await this.mailgun.messages.create(process.env.MAILGUN_DOMAIN, {
+      from: this.from,
+      to: this.to,
+      subject: this.subject,
+      text: this.text,
+      html: this.html,
+    });
+
+    return this;
   }
 }
 
---
+++ utils/Logger.js
@@ -0,0 +1,38 @@
+const Log = require("../models/database/Log");
+
+class Logger {
+  static async info(description, data) {
+    this.emit("info", description, data);
+  }
+
+  static async warning(description, data) {
+    this.emit("warning", description, data);
+  }
+  static async error(description, data) {
+    this.emit("error", description, data);
+  }
+  static async alert(description, data) {
+    this.emit("alert", description, data);
+  }
+
+  static async emit(type, description, data) {
+    if (process.env.NODE_ENV !== "production") {
+      let method = "error";
+      if (type === "info") method = "log";
+      if (type === "warning") method = "warn";
+      console[method](`[${type.toUpperCase()}] ${description}`);
+    }
+
+    await Log.create({ type, description, data });
+  }
+
+  /**
+   * Logs message in all environments
+   */
+  static announce(message) {
+    console.log(`[LOG] ${message}`);
+  }
+}
+
+const logger = Logger;
+module.exports = logger;
