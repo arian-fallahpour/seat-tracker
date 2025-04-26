59569fd2aad28dcc5c97b514b560798010956f18
JinXzAF <arianf2004@gmail.com>
2025-04-26 17:21:08 -0400

    added production key for stripe

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
 const config = {
   API_PATH: "api/v1",
   STRIPE_PUBLIC_KEY:
-    "pk_test_51QvA1yGLcRGY5lHyD8lxAaGNbDAiVKXYnXN6rg0SPeVeUg2WcANdSJ4B1R7w1LyGSvZiofVhOALQoJXzOCFfYGm7008j66Tjbu",
+    process.env.NODE_ENV === "development"
+      ? "pk_test_51QvA1yGLcRGY5lHyD8lxAaGNbDAiVKXYnXN6rg0SPeVeUg2WcANdSJ4B1R7w1LyGSvZiofVhOALQoJXzOCFfYGm7008j66Tjbu"
+      : "pk_live_51QvA1yGLcRGY5lHyKSOjhcLD8Zaaa4FZyJWhtHCpp7SdWN810szHak0yeyUy7he0aCD0YikPNlrePhIhIIA996pf00llsmS19Z",
 };
 
 module.exports = config;
