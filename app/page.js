e185dc9f916243309424fd926381cd02edb47e4b
JinXzAF <arianf2004@gmail.com>
2025-05-09 16:50:09 -0400

    changed title

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import HomePage from "@/components/pages/HomePage/HomePage";
 
+export const metadata = {
+  title: getPageHeader("Create waitlisted alerts for your courses!"),
+};
+
 export default async function Page() {
   return <HomePage />;
 }
