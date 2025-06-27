98c0edbb028903b65065a01b5e61846ac0b24d14
JinXzAF <arianf2004@gmail.com>
2025-06-27 13:04:20 -0400

    Fixed import bug

--- scripts/import-uoft-courses.js
+++ scripts/import-uoft-courses.js
@@ -3,6 +3,11 @@
  *
  * This script fetches all of the uoft courses based on the year and season
  * and upserts them into the database
+ *
+ * e.g. node scripts/import-uoft-courses.js --season=fall-winter --year=2025 --upsert=100
+ *      The only seasons are fall-winter and summer
+ *      The year is the year of the fall semester, e.g. 2025 for fall-winter 2025
+ *      Upsert is the number of courses to upsert at a time, default is 500
  */
 
 const dotenv = require("dotenv");
@@ -37,14 +42,22 @@ const importData = async () => {
   const year = args.year;
   const limit = args.limit || -1;
   const upsert = args.upsert || 500;
+  let maxSkips = args.skips || 5;
 
   let page = 0;
+  let skips = 0;
   while (page != limit) {
+    if (skips >= maxSkips) {
+      console.log(`[INFO] Skipped ${maxSkips} pages, stopping import`);
+      break;
+    }
+
     console.log(`[INFO] Requesting UofT API (page: ${++page})`);
 
     // Fetch courses from school API
     const fetchedCourses = await UoftAdapter.fetchCourses({ page, season, year });
-    if (fetchedCourses.length === 0) break;
+    if (fetchedCourses.length === 0) skips++;
+    else skips = 0;
 
     updatedCourses.push(...fetchedCourses);
   }
